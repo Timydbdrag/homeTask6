07786b3d8761b71a5d400b59af1f53f49ea9b9d6
Tim <timydbdrag@mail.ru>
2021-05-07 14:08:29 +0300

    fixik

--- src/main/scala/Consumer.scala
+++ src/main/scala/Consumer.scala
@@ -6,9 +6,7 @@ import scala.jdk.CollectionConverters._
 
 object Consumer {
 
-  private val TOPIC= "books"
-  private val PARTITION_COUNT = 3
-  private val MSG_COUNT = 5
+  private val TOPIC = "books"
 
   def main(args: Array[String]): Unit = {
     val consumer = KafkaHelper.createConsumer()
@@ -21,29 +19,26 @@ object Consumer {
     var numMsgReceived = 0
 
     import scala.util.control.Breaks._
-    while (true){
-      val records = consumer.poll(Duration.ofSeconds(5)).asScala
+    breakable {
+      while (true) {
+        val records = consumer.poll(Duration.ofSeconds(1)).asScala
 
-      for (r <- records) {
-        numMsgReceived += 1
-        if (r.partition() == 0) myQueue1.add("partition: " + r.partition() + ", offset: " + r.offset() + ", val: " + r.value())
-        if (r.partition() == 1) myQueue2.add("partition: " + r.partition() + ", offset: " + r.offset() + ", val: " + r.value())
-        if (r.partition() == 2) myQueue3.add("partition: " + r.partition() + ", offset: " + r.offset() + ", val: " + r.value())
-      }
-      if(myQueue1.get.nonEmpty || myQueue2.get.nonEmpty || myQueue3.get.nonEmpty) {
-        println("=" * 40)
-        myQueue1.get.foreach(println)
-        myQueue2.get.foreach(println)
-        myQueue3.get.foreach(println)
-      }
+        if (records.isEmpty) break
 
-      consumer.commitSync()
-
-      if (numMsgReceived == MSG_COUNT * PARTITION_COUNT) {
-        println("exit")
-        break()
+        for (r <- records) {
+          numMsgReceived += 1
+          if (r.partition() == 0) myQueue1.add("partition: " + r.partition() + ", offset: " + r.offset() + ", val: " + r.value())
+          if (r.partition() == 1) myQueue2.add("partition: " + r.partition() + ", offset: " + r.offset() + ", val: " + r.value())
+          if (r.partition() == 2) myQueue3.add("partition: " + r.partition() + ", offset: " + r.offset() + ", val: " + r.value())
+        }
+        //consumer.commitSync()
       }
     }
-
+    if (myQueue1.get.nonEmpty || myQueue2.get.nonEmpty || myQueue3.get.nonEmpty) {
+      println("=" * 40)
+      myQueue1.get.foreach(println)
+      myQueue2.get.foreach(println)
+      myQueue3.get.foreach(println)
+    }
   }
 }
--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -6,20 +6,18 @@ import service.{KafkaHelper, TestCallback}
 
 object Producer {
 
-  private val TOPIC="books"
+  private val TOPIC = "books"
   private val PARTITION_COUNT = 3
 
   def main(args: Array[String]): Unit = {
-    val producer:KafkaProducer[String,String] = KafkaHelper.createProducer()
+    val producer: KafkaProducer[String, String] = KafkaHelper.createProducer()
 
     val callback = new TestCallback
     val dt = getData
 
-    for(part <- 0 until PARTITION_COUNT){
-      dt.forEach(el => {
-        producer.send(new ProducerRecord(TOPIC, part, Integer.toString(part), el.asJson.spaces2), callback)
-      })
-    }
+    dt.forEach(el => {
+      producer.send(new ProducerRecord(TOPIC, el.asJson.spaces2), callback)
+    })
 
     producer.close()
   }
